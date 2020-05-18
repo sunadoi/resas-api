b3d8228e39fc9d50a69c70b84676f4da7d01419d
上野弘敬 <hiroyukiueno@hiroyukiueno.local>
2020-05-18 18:53:39 +0900

    first commit

--- src/Chart.js
+++ src/Chart.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
 import APIKEY from "./Apikey";
@@ -32,103 +32,6 @@ class Chart extends Component {
         this.setState({ prefectures: res.result });
       })
       .catch((error) => console.error(error));
-  }
-
-  _changeSelection(index) {
-    const selected_copy = this.state.selected.slice();
-    selected_copy[index] = !selected_copy[index];
-
-    if (!this.state.selected[index]) {
-      fetch(
-        `https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=-&prefCode=${index}`,
-        { headers: { "X-API-KEY": APIKEY } }
-      )
-        .then((response) => response.json())
-        .then((res) => {
-          const tmp = [];
-          const tmpYears = [];
-          // Object.keys(res.result.data[0]).forEach((i) => {
-          res.result.data[0].data.forEach((value, i) => {
-            // console.log(i);
-            // console.log(value);
-
-            console.log(res.result.data[0].data[i] === value);
-            tmp.push(value["value"]);
-            tmpYears.push(value["year"]);
-          });
-          console.log(tmp, tmpYears);
-
-          console.log(this.state.series);
-          const res_series = {
-            name: this.state.prefectures[index].prefName,
-            data: tmp,
-          };
-          this.setState({
-            selected: selected_copy,
-            series: [...this.state.series, res_series],
-            categories: tmpYears,
-          });
-        });
-    } else {
-      // チェック済みの場合は削除
-      const series_copy = this.state.series.slice();
-
-      for (let i = 0; i < series_copy.length; i++) {
-        if (series_copy[i].name == this.state.prefectures[index].prefName) {
-          series_copy.splice(i, 1);
-        }
-      }
-      this.setState({
-        selected: selected_copy,
-        series: series_copy,
-      });
-    }
-  }
-
-  renderItem(props) {
-    return (
-      <div
-        key={props.prefCode}
-        style={{ margin: "5px", display: "inline-block" }}
-      >
-        <input
-          type="checkbox"
-          checked={this.state.selected[props.prefCode - 1]}
-          onChange={() => this._changeSelection(props.prefCode - 1)}
-        />
-        {props.prefName}
-      </div>
-    );
-  }
-
-  render() {
-    const obj = this.state.prefectures;
-    const options = {
-      title: {
-        text: "都道府県",
-      },
-      plotOptions: {
-        series: {
-          label: {
-            connectorAllowed: false,
-          },
-          pointInterval: 10,
-          pointStart: 1980,
-        },
-      },
-      xAxis: {
-        categories: this.state.categoeis,
-      },
-      series: this.state.series,
-    };
-    return (
-      <div style={{ margin: "30px" }}>
-        <h1>都道府県別の総人口推移グラフ</h1>
-
-        {Object.keys(obj).map((i) => this.renderItem(obj[i]))}
-        <HighchartsReact highcharts={Highcharts} options={options} />
-      </div>
-    );
   }
 }
 
